2ceeb7f30ed6c97e264638ed85f6a55464f46ac5
CailleauThierry <thierrycailleau@yahoo.fr>
2016-07-26 23:03:37 -0700

    Collection of PowerShell utilities
    I did a slight renaming of my repository so the previous commit comment
    did not get copied over. That is fine, those are very early days
    still...

---
+++ Invoke-CsvEvtx2TxtErrWg/Invoke-CsvEvtx2TxtErrWg.ps1
@@ -0,0 +1,77 @@
+﻿<#
+	.SYNOPSIS
+		Invoke-CsvEvtx2TxtErrWg.ps1 based on Invoke-CSV_EVTX2TxtErrWg_03.ps1 allwoing to filter out cluter messages
+
+	.DESCRIPTION
+	    Takes any *.csv or *.evtx files and filters Warning and errors out of them for at least the past year
+
+
+	.PARAMETER  ParameterA
+		The description of the ParameterA parameter.
+
+	.PARAMETER  ParameterB
+		The description of the ParameterB parameter.
+
+	.EXAMPLE
+		PS C:\> Get-Something -ParameterA 'One value' -ParameterB 32
+
+	.EXAMPLE
+		PS C:\> Get-Something 'One value' 32
+
+	.INPUTS
+		System.String,System.Int32
+
+	.OUTPUTS
+		System.String
+
+	.NOTES
+		Additional information about the function go here.
+
+	.LINK
+		about_functions_advanced
+
+	.LINK
+		about_comment_based_help
+
+#>
+
+
+param ( 
+[Parameter(mandatory=$true)] [string]$CsvEvtxFullName
+)
+		
+$filename = ($CsvEvtxFullName).Split('\\')[-1]
+$filedirectory =  $CsvEvtxFullName.TrimEnd("$filename")
+
+
+$lastyear = (get-date).AddMonths(-12).Year
+
+if (($filename) -like "*.csv")
+{
+	Write-Host "Filtering: `n $CsvEvtxFullName"
+
+	Get-ChildItem $CsvEvtxFullName | ForEach-Object { 		
+		Write-Output "Application Event Errors and Warnings--------------------------------------------------------------------------------------------------------------------------------" | Out-File ("$filedirectory" + "$filename" + "_filtered.txt") -NoClobber
+		Import-Csv $CsvEvtxFullName | where {$_.'Date and Time'.Replace('-'," ").Replace('/',' ').Split(" ")[2] -ge "$lastyear"}  | where {($_.LevelDisplayName -like "Error" -or $_.LevelDisplayName -like "Warning") -and ($_.Message -notmatch ".*OneNote.*|.*CutePDF.*|.*Xerox.*|.*LaserJet.*|.*Microsoft XPS.*|.*printer.*")}  | Out-File ("$filedirectory" + "$filename" + "_filtered.txt") -Append
+	Write-Host "Filtered to:"
+	Write-Host ("$filedirectory" + "$filename" + "_filtered.txt").ToString()
+	}
+}
+ 
+elseif(($filename) -like "*.evtx")
+{ 
+	Write-Host "Filtering: `n $CsvEvtxFullName"
+	$GWEAppEvtx = Get-WinEvent -Path $CsvEvtxFullName
+	Write-Output "Event Errors or Warnings ---------------------------------------------------------------------------------------------------------------------------------" | Out-File  ("$filedirectory" + "$filename" + "_filtered.txt") -NoClobber
+	$GWEAppEvtx | where {$_.TimeCreated.ToString().Replace('-'," ").Replace('/',' ').Split(" ")[2] -ge "$lastyear"} | where {($_.LevelDisplayName -like "Error" -or $_.LevelDisplayName -like "Warning") -and ($_.Message -notmatch ".*OneNote.*|.*CutePDF.*|.*Xerox.*|.*LaserJet.*|.*Microsoft XPS.*|.*printer.*")}   |  Format-List -Property LevelDisplayName,TimeCreated,ProviderName,Id,Keywords,ProcessId,MachineName,UserId,Message  | Out-File  ("$filedirectory" + "$filename" + "_filtered.txt") -Width 300 -Append
+	Write-Host "Filtered to:"
+	Write-Host ("$filedirectory" + "$filename" + "_filtered.txt").ToString()
+	
+}
+
+else
+{
+Write-Host "$filename was not a *.csv or *.evtx file"
+}
+
+
---
+++ Outlook/Get-OutlookAttachment.ps1
@@ -0,0 +1,34 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 From: https://social.technet.microsoft.com/Forums/windowsserver/en-US/11218cf2-eb24-4110-967f-b29234064501/how-do-i-save-outlook-attachments-using-powershell?forum=winserverpowershell
+	 Created on:   	7/6/2016 1:18 PM
+	 Created by:   	CailleauThierry
+	 Organization: 	Private
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+# <-- Script --------->
+#requires -Version 2
+$o = New-Object -comobject outlook.application
+$n = $o.GetNamespace("MAPI")
+$f = $n.PickFolder()
+$filepath = "c:\temp\"
+$f.Items | foreach {
+	$SendName = $_.SenderName
+	$_.attachments | foreach {
+		Write-Host $_.filename
+		$a = $_.filename
+		If ($a.Contains("xlsx"))
+		{
+			$_.saveasfile((Join-Path $filepath "$SendName.xlsx"))
+		}
+	}
+}
+# <------ End Script ---------------------------------->
+#me: this works great with PS v5 and v2. It only requires you frist create a C:\temp\ folder (this could be added to the script)
+#It then asks you to select the folder you want ot scan (I selected the Inbox) folder.
+#The script "Write-Host" actually display all attachments (.png .jpg) so the "If ($a.Contains("xlsx"))" part of it line 24 could be chnage to the different extensions you need,
+#	you could even replace with If ($a -match "xlsx") for regex matching of different extension types
