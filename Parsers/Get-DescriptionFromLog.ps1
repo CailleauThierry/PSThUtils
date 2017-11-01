f3588da0b6ebc88f0b9e516b9e727e0f55f0e36b
CailleauThierry <thierrycailleau@yahoo.fr>
2017-11-01 20:03:37 +0100

    "-" tdo not need to be escaped in regex

--- Parsers/Get-DescriptionFromLog.ps1
+++ Parsers/Get-DescriptionFromLog.ps1
@@ -61,7 +61,7 @@ $log1 = Get-Content $InputFile
 $Log.LogPath = $log1[1].PSPath
 $Log.LogName = $log1[1].PSChildName
 
-if (($log1[1]) -match '(^)((\d{2}\-\w{3}-\d{4}\s))')
+if (($log1[1]) -match '(^)((\d{2}-\w{3}-\d{4}\s))')
 {
 	# matching Vault 8.x log formating
 	#Log object definition also defines in what order the objects will be displayed at the end
@@ -78,15 +78,15 @@ if (($log1[1]) -match '(^)((\d{2}\-\w{3}-\d{4}\s))')
 	
 	# key0 : line identifier key1 : RegEx Expression Matching for key0 identifier key2 is the PSObject Property Name associated with key0 identifier
 	$A0 = @{key0 = '(^)(\d{2}-\w{3}-\d{4})';key1 = '(^)(?<RegExMatch>(\d{2}-\w{3}-\d{4}\s\d{2}:\d{2}:\d{2}))';key2 = 'LogEndTime'}  	# '(^)(?<RegExMatch>(\w{3,4}\d{2}\s\d{2}:\d{2}:\d{2}))' matching format 'May22 21:19:03'
-	$A1 = @{key0 = '\-I\-0354';key1 = '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = 'AgentVersion'}  					# '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))' matching format 'VVLT-I-0354 Agent version is <7.50.6422>'
-	$A2 = @{key0 = '\-I\-0219';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = 'VaultVersion'}  							# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
+	$A1 = @{key0 = '-I-0354';key1 = '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = 'AgentVersion'}  					# '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))' matching format 'VVLT-I-0354 Agent version is <7.50.6422>'
+	$A2 = @{key0 = '-I-0219';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = 'VaultVersion'}  							# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
 	$A3 = @{key0 =  'hn =';key1 = '(hn = )(?<RegExMatch>(.*?))($)';key2 = 'HostName'} 										# '(hn =)(?<RegExMatch>(.*?))($)' matching format 'hn = 1_host_name 
 	$A4 = @{key0 =  'ip =';key1 = '(ip = )(?<RegExMatch>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))';key2 = 'IPAddress'} 			# '(ip=)' is not needed here but it looks consistent with previous (hn=)
 	$A5 = @{key0 =  ' tn = ';key1 = '(tn = )(?<RegExMatch>(.*))';key2 = 'TaskName'}											# '(tn = )(?<RegExMatch>(.*))' matching format 'tn = 1_task_name'
 	$A6 = @{key0 =  ' tid= ';key1 = '(tid= .*  \()(?<RegExMatch>([\d]{1,3}))';key2 = 'TaskNumber'}							# '(tid= .*  \()(?<RegExMatch>([\d]{1,3}))' matching format 'tid= e69994fd-fd03-4a15-b645-0c7097760595  (2)' by counting 1 "single space" i.e '[\s]' and then 2 single space i.e. [\s]{2} then checking for opening parentheses '\('  where '.*' stands for "anycharacters in between"
-	$A7 = @{key0 = ' tid=';key1 = '(tid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'TaskGUID'}
-	$A8 = @{key0 = ' cid=';key1 = '(cid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'AgentGUID'}  				# there is no "," at the end of the first cid
-	$A9 = @{key0 = ' vid=';key1 = '(vid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'VaultNameIPOrFQDN'}  				# since guid are a set format, I do not need to match the "," at the end
+	$A7 = @{key0 = ' tid=';key1 = '(tid= )(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'TaskGUID'}
+	$A8 = @{key0 = ' cid=';key1 = '(cid= )(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'AgentGUID'}  				# there is no "," at the end of the first cid
+	$A9 = @{key0 = ' vid=';key1 = '(vid= )(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'VaultNameIPOrFQDN'}  				# since guid are a set format, I do not need to match the "," at the end
 	}
 
 elseif (($log1[1]) -match '(^)(\w{3}\d{2})')
@@ -106,18 +106,18 @@ elseif (($log1[1]) -match '(^)(\w{3}\d{2})')
 	
 	# key0 : line identifier key1 : RegEx Expression Matching for key0 identifier key2 is the PSObject Property Name associated with key0 identifier
 	$A0 = @{key0 = '(^)(\w{3}\d{2})';key1 = '(^)(?<RegExMatch>(\w{3,4}\d{2}\s\d{2}:\d{2}:\d{2}))';key2 = 'LogEndTime'}  	# '(^)(?<RegExMatch>(\w{3,4}\d{2}\s\d{2}:\d{2}:\d{2}))' matching format 'May22 21:19:03'
-	$A1 = @{key0 = '\-I\-0354';key1 = '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = 'AgentVersion'}  					# '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))' matching format 'VVLT-I-0354 Agent version is <7.50.6422>'
-	$A2 = @{key0 = '\-I\-0219';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = 'VaultVersion'}  							# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
+	$A1 = @{key0 = '-I-0354';key1 = '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = 'AgentVersion'}  					# '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))' matching format 'VVLT-I-0354 Agent version is <7.50.6422>'
+	$A2 = @{key0 = '-I-0219';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = 'VaultVersion'}  							# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
 	$A3 = @{key0 =  'hn =';key1 = '(hn = )(?<RegExMatch>(.*?))($)';key2 = 'HostName'} 										# '(hn =)(?<RegExMatch>(.*?))($)' matching format 'hn = 1_host_name 
 	$A4 = @{key0 =  'ip =';key1 = '(ip = )(?<RegExMatch>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))';key2 = 'IPAddress'} 			# '(ip=)' is not needed here but it looks consistent with previous (hn=)
 	$A5 = @{key0 =  ' tn = ';key1 = '(tn = )(?<RegExMatch>(.*))';key2 = 'TaskName'}											# '(tn = )(?<RegExMatch>(.*))' matching format 'tn = 1_task_name'
 	$A6 = @{key0 =  ' tid= ';key1 = '(tid= .*  \()(?<RegExMatch>([\d]{1,3}))';key2 = 'TaskNumber'}							# '(tid= .*  \()(?<RegExMatch>([\d]{1,3}))' matching format 'tid= e69994fd-fd03-4a15-b645-0c7097760595  (2)' by counting 1 "single space" i.e '[\s]' and then 2 single space i.e. [\s]{2} then checking for opening parentheses '\('  where '.*' stands for "anycharacters in between"
-	$A7 = @{key0 = ' tid=';key1 = '(tid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'TaskGUID'}
-	$A8 = @{key0 = ' cid=';key1 = '(cid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'AgentGUID'}  				# there is no "," at the end of the first cid
-	$A9 = @{key0 = ' vid=';key1 = '(vid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'VaultNameIPOrFQDN'}  				# since guid are a set format, I do not need to match the "," at the end
+	$A7 = @{key0 = ' tid=';key1 = '(tid= )(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'TaskGUID'}
+	$A8 = @{key0 = ' cid=';key1 = '(cid= )(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'AgentGUID'}  				# there is no "," at the end of the first cid
+	$A9 = @{key0 = ' vid=';key1 = '(vid= )(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'VaultNameIPOrFQDN'}  				# since guid are a set format, I do not need to match the "," at the end
 	}
 
-elseif (($log1[1]) -match '(^)((\d{2}\-\w{3}\s))')
+elseif (($log1[1]) -match '(^)((\d{2}-\w{3}\s))')
 {
 	# matching Agent 7.x and 8.x log formating
 	#Log object definition also defines in what order the objects will be displayed at the end
@@ -133,16 +133,16 @@ elseif (($log1[1]) -match '(^)((\d{2}\-\w{3}\s))')
 	$Log | Add-Member NoteProperty VaultNameIPOrFQDN ' Vault Name IP or FQDN'
 	
 	# key0 : line identifier key1 : RegEx Expression Matching for key0 identifier key2 is the PSObject Property Name associated with key0 identifier
-	$A0 = @{key0 = '(^)((\d{2}\-\w{3}))';key1 = '(^)(?<RegExMatch>(\d{2}\-\w{3,4}\s\d{2}:\d{2}:\d{2}))';key2 = 'LogEndTime'}  	# '(^)(?<RegExMatch>(\d{2}\-\w{3}\s\d{2}:\d{2}:\d{2}))' matching format '04-Dec 21:30:02'
-	$A1 = @{key0 = '\-I\-04314';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = 'AgentVersion'}  						# " BKUP-I-04314" same code as" REST-I-04314" so chnaging it to "-I-04314" only
-	$A2 = @{key0 = '\-I\-04315';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = 'VaultVersion'}  								# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
+	$A0 = @{key0 = '(^)((\d{2}-\w{3}))';key1 = '(^)(?<RegExMatch>(\d{2}-\w{3,4}\s\d{2}:\d{2}:\d{2}))';key2 = 'LogEndTime'}  	# '(^)(?<RegExMatch>(\d{2}-\w{3}\s\d{2}:\d{2}:\d{2}))' matching format '04-Dec 21:30:02'
+	$A1 = @{key0 = '-I-04314';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = 'AgentVersion'}  						# " BKUP-I-04314" same code as" REST-I-04314" so chnaging it to "-I-04314" only
+	$A2 = @{key0 = '-I-04315';key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = 'VaultVersion'}  								# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
 	$A3 = @{key0 =  ' hn=';key1 =  '(hn=)(?<RegExMatch>(.*?))[,\s]\s*';key2 = 'HostName'} 										# RegEx tested on http://rubular.com/ (.*?) where "?" means relunctant (matches only once) as oppose to greedy. See http://groovy.codehaus.org/Tutorial+5+-+Capturing+regex+groups> 
 	$A4 = @{key0 =  ' ip=';key1 = '(ip=)(?<RegExMatch>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))';key2 = 'IPAddress'} 				# '(ip=)' is not needed here but it looks consistent with previous (hn=)
 	$A5 = @{key0 =  ' tn=';key1 =  '(tn=)(?<RegExMatch>(.*?))[,\s]\s*';key2 = 'TaskName'}
-	$A6 = @{key0 =  '\-I\-04133';key1 = '(\-I\-04133.*[\s])(?<RegExMatch>(\d+))';key2 = 'SafesetNumber'}					# '\-I\-04133[\s].*)(?<RegExMatch>([\s]\d+))' matching format  '-I-04132 synching catalog number is XXX' in any languages
-	$A7 = @{key0 = ' tid=';key1 = '(tid=)(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'TaskGUID'}
-	$A8 = @{key0 = ' cid=';key1 = '(cid=)(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = 'AgentGUID'}  				# there is no "," at the end of the first cid
-	$A9 = @{key0 = '\-I\-04746';key1 = '(SSET\-I\-04746.*\s)(?<RegExMatch>(.*)),';key2 = 'VaultNameIPOrFQDN'}  				# since guid are a set format, I do not need to match the "," at the end
+	$A6 = @{key0 =  '-I-04133';key1 = '(-I-04133.*[\s])(?<RegExMatch>(\d+))';key2 = 'SafesetNumber'}					# '-I-04133[\s].*)(?<RegExMatch>([\s]\d+))' matching format  '-I-04132 synching catalog number is XXX' in any languages
+	$A7 = @{key0 = ' tid=';key1 = '(tid=)(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'TaskGUID'}
+	$A8 = @{key0 = ' cid=';key1 = '(cid=)(?<RegExMatch>(\w{8}-\w{4}-\w{4}-\w{4}-\w{12}))';key2 = 'AgentGUID'}  				# there is no "," at the end of the first cid
+	$A9 = @{key0 = '-I-04746';key1 = '(SSET-I-04746.*\s)(?<RegExMatch>(.*)),';key2 = 'VaultNameIPOrFQDN'}  				# since guid are a set format, I do not need to match the "," at the end
 	
 	} 
 	else
