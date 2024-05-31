0d753394913db1ba632264709d576f5a511bbbe0
CailleauThierry <thierrycailleau@yahoo.fr>
2024-05-31 08:59:25 +0200

    Get-MSINFOTOO.ps1 is now replaced by Set-ScriptLauncher.ps1 with $Selector = "Get-MSInfo"

--- Parsers/Get-MSINFOTOO.ps1
+++
@@ -1,4 +0,0 @@
-# Get-MSinfo.ps1 now resets arrays to allow for multiple .nfo to be handled. Get-MSINFOTOO.ps1 passes those multipe *.nfo files to it.
-
-. $env:HOMEPATH\Documents\WindowsPowerShell\Scripts\PSThUtils\Parsers\Get-MSInfo.ps1
-Get-ChildItem C:\Temp\06xxxxxx\2024\*.nfo -Filter *.nfo -Recurse | Select-Object -Property FullName | Get-MSInfo   
